9fcb238b3ebe32c841d60ccbfe149026e1497585
Joric <51609531+jonathanadamrico@users.noreply.github.com>
2021-06-12 18:46:03 +0800

    Include deletions (mutations)

--- classes/Sequence.py
+++ classes/Sequence.py
@@ -95,15 +95,15 @@ class Sequence:
       
       # if no difference and not the reference sequence
       if ref_protein[i] == self.protein[i] and not(is_ref) :
-        self.mutations.append('-')
+        self.mutations.append('=')
 
       # if proteins are different or if it is the reference sequence
       # technically there should be no difference, but based on the requested output,
       # we would like to see the reference sequence on top of the differences
       else:
         self.mutations.append(self.protein[i])
-        # Assuming that 'X, -, _' are not a real mutations
-        if self.protein[i] not in ['X', '-', '_'] and not(is_ref):
+        # Assuming that 'X, '_' are not a real mutations
+        if self.protein[i] not in ['X', '_'] and not(is_ref):
           protein_diff = ref_protein[i] + str(i+1) + self.protein[i]
           df.loc[int(df[df['ID']==name].index[0]), start_loc + i + 1] = protein_diff
           #print('{} | {} | {}'.format(int(df[df['ID']==name].index[0]), start_loc + i + 1, protein_diff))
